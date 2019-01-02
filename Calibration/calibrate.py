e4a836db4162ef3d7278ba111208e15906d61dd3
Omar Padierna <omar@aerialytic.com>
2019-01-02 17:24:44 -0600

    Author and license added
    Author and license added

--- Calibration/calibrate.py
+++ Calibration/calibrate.py
@@ -1,3 +1,19 @@
+'''
+Created by Omar Padierna "Para11ax" on Jan 1 2019
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+'''
+
+
 import cv2
 import numpy as np 
 import glob
